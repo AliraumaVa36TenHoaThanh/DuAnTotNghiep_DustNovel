3a2d302b2a7f2fda289ec491c991ad5afc832448
AliraumaVa36TenHoaThanh <buih0agganh@gmail.com>
2026-02-24 19:35:26 +0700

    Update TruyenController.java

--- src/main/java/com/fpoly/controller/TruyenController.java
+++ src/main/java/com/fpoly/controller/TruyenController.java
@@ -237,7 +237,7 @@ public class TruyenController {
 
 	        truyenService.save2(truyen);
 	        return "redirect:/DustNovel/truyen/" + id;
-	    
+	    }  
 	    @GetMapping("/the-loai/{id}")
 	    public String xemTheoTheLoai(@PathVariable Long id, Model model) {
 
