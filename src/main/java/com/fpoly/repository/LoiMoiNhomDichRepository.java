9645dfdf13afc931b70a6600ccade80f6b014cce
Cxnnie <sephilul28@gmail.com>
2026-02-27 13:37:20 +0700

    1/2 guild
    guild

--- src/main/java/com/fpoly/repository/LoiMoiNhomDichRepository.java
+++ src/main/java/com/fpoly/repository/LoiMoiNhomDichRepository.java
@@ -1,10 +1,33 @@
 package com.fpoly.repository;
 
 import com.fpoly.model.LoiMoiNhomDich;
+import com.fpoly.model.NguoiDung;
+import com.fpoly.model.NhomDich;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface LoiMoiNhomDichRepository extends JpaRepository<LoiMoiNhomDich, Long> {
-    List<LoiMoiNhomDich> findByNguoiDuocMoiIdAndTrangThai(Long userId, String trangThai);
-    boolean existsByNhomDichIdAndNguoiDuocMoiId(Long nhomId, Long userId);
+
+    Optional<LoiMoiNhomDich> 
+    findByNhomDichAndNguoiMoi(NhomDich nhomDich, NguoiDung nguoiMoi);
+
+    List<LoiMoiNhomDich> 
+    findByNguoiDuocMoiAndTrangThai(NguoiDung nguoiDuocMoi, String trangThai);
+    
+    Optional<LoiMoiNhomDich> 
+    findByNhomDichAndNguoiDuocMoi(NhomDich nhom, NguoiDung user);
+    List<LoiMoiNhomDich> findByNhomDichAndTrangThai(
+            NhomDich nhomDich,
+            String trangThai
+    );
+
+    Optional<LoiMoiNhomDich> findByNhomDichAndNguoiMoiAndTrangThai(
+            NhomDich nhom, 
+            NguoiDung user, 
+            String trangThai
+    );
+    
+    boolean existsByNhomDichAndNguoiMoi(NhomDich nhom, NguoiDung nguoiMoi);
 }
--- src/main/java/com/fpoly/repository/ThanhVienNhomDichRepository.java
+++ src/main/java/com/fpoly/repository/ThanhVienNhomDichRepository.java
@@ -1,9 +1,21 @@
 package com.fpoly.repository;
 
 import com.fpoly.model.ThanhVienNhomDich;
+import com.fpoly.model.NguoiDung;
+import com.fpoly.model.NhomDich;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ThanhVienNhomDichRepository extends JpaRepository<ThanhVienNhomDich, Long> {
     Optional<ThanhVienNhomDich> findByNhomDichIdAndNguoiDungId(Long nhomId, Long userId);
+    List<ThanhVienNhomDich> findByNhomDich(NhomDich nhomDich);
+    List<ThanhVienNhomDich> 
+    findByNhomDichAndTrangThai(NhomDich nhomDich, String trangThai);
+
+
+    Optional<ThanhVienNhomDich> 
+    findByNhomDichAndNguoiDung(NhomDich nhom, NguoiDung nguoiDung);
+
 }
