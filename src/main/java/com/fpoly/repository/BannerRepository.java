5a83cfbb981d1371279350dbdcb2270c3770b993
LÊ QUỐC ANH <lequocanh141007@gmail.com>
2026-04-12 21:34:15 +0700

    4

--- src/main/java/com/fpoly/repository/BannerRepository.java
+++ src/main/java/com/fpoly/repository/BannerRepository.java
@@ -1,7 +1,13 @@
 package com.fpoly.repository;
 
 import com.fpoly.model.Banner;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -30,5 +36,14 @@ public interface BannerRepository extends JpaRepository<Banner, Long> {
     
     List<Banner> findByTruyenIdInAndTruyen_TenTruyenContainingIgnoreCase(
             List<Long> ids, String keyword);
+    
+    boolean existsByTruyenId(Long truyenId);
+    
+    @Modifying
+    @Transactional
+    void deleteByTruyenId(Long truyenId);
+    
+    @Query("SELECT b FROM Banner b WHERE b.truyen.id = :truyenId")
+    List<Banner> findByTruyenId(@Param("truyenId") Long truyenId);
 
 }
